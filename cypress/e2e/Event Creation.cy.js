06221e52da4b7fd4c7ca6f43035e4566492984fc
vignesh <vigneshs@leaguex.com>
2025-01-23 14:19:59 +0530

    new changee

--- cypress/e2e/Event Creation.cy.js
+++ cypress/e2e/Event Creation.cy.js
@@ -11,7 +11,7 @@ describe('Event Creation', () => {
     });
   });
 
-  // Single Event Creation Test
+  //Single Event Creation Test
   it('Single Event Creation', () => {
     const eventOne = eventData.event1;
     const eventstartDate = eventData.event1.startDate; 
@@ -101,4 +101,31 @@ describe('Event Creation', () => {
     cy.get('input[class="PrivateSwitchBase-input css-1m9pwf3"]').click();
     cy.contains("button", "SUBMIT").click();
   });
+
+    //Multiple Inventory Event Creation
+    it('Multiple Inventory Event Creation', () => {
+        const eventFive = eventData.event5;
+        const eventstartDate = eventData.event5.startDate; 
+        const eventendDate = eventData.event5.endDate;    
+        const eventcloseDate = eventData.event5.closeDate
+        cy.contains("button", "Add Event").click();
+        cy.get('input[placeholder="Enter Event Name"]').type(eventFive.eventName);
+        cy.get('#start_date').click();
+        selectDate(eventstartDate);
+        cy.get('#end_date').click();
+        selectDate(eventendDate);
+        cy.get('#close_date').click();
+        selectDate(eventcloseDate);
+        cy.contains("label", "Event Type*").parent().find('input[type="text"]').type(eventFive.eventType).wait(500).type("{enter}");
+        cy.contains("label", "Inventory*").parent().find('input[id="inventoryInput0"]').type(eventFive.inventory1).wait(5000).type("{enter}");
+        cy.contains("label", "Select Project owner(s)*").eq(0).parent().click().find('input[type="search"]').type(eventFive.projectOwner1).wait(5000).type("{enter}")
+        cy.get('input[class="PrivateSwitchBase-input css-1m9pwf3"]').click();
+        cy.contains('h1', 'Inventories Details').click();
+        cy.contains("button", "Add another Inventory").click()
+        cy.get('#inventoryInput1').click().type(eventFive.inventory2).wait(5000).type("{enter}");
+        cy.contains('span', 'Select Project owner(s)').click();
+        cy.get('#default-search').should('be.visible').type(eventFive.projectOwner2).wait(5000).type('{enter}');
+        cy.get('input[class="PrivateSwitchBase-input css-1m9pwf3"]').click();
+        cy.contains("button", "SUBMIT").click();
+      });
 });
--- cypress/support/LoginITWCRM.js
+++ cypress/support/LoginITWCRM.js
@@ -9,7 +9,7 @@ export default function performLogin(){
           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NjIsInVzZXJfaWQiOjYyLCJ1c2VyRW1haWwiOiJiZWFzdEB0aGVhbHRlcm9mZmljZS5jb20iLCJ1c2VyTmFtZSI6IkJlYXN0IEFrc2hheSIsImRldmljZUlkIjoiIiwicm9sZXMiOlsiQURNSU4iLCJQTyJdLCJpYXQiOjE3MzMzODE0NzZ9.OrdV21jTVfpT2Thdcmhl1Errh5SKauGe4PSfZ57hHGY",
       },
       body: {                  // Data sent in the request body
-        "userEmail": "punith@thealteroffice.com"
+        "userEmail": "beast@thealteroffice.com"
       },
     }).then((response) => {
       cy.get("input").type(response.body.token);
